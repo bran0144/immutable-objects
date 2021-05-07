8d74108997dc721cc5f9d692b87c2ee1176cd380
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-05-07 11:48:15 -0500

    fixed bug

--- src/com/target/immutableobjects/Main.java
+++ src/com/target/immutableobjects/Main.java
@@ -1,5 +1,5 @@
 package com.target.immutableobjects;
 
 public class Main {
-    public static void main(String[] args) {new Demo().run;}
+    public static void main(String[] args) { new Demo().run(); }
 }
